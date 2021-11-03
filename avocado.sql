d8f756cde47a69d6c5c5b5897176c8a0d0069d81
TrentBrunson <bbiz2@icloud.com>
2021-11-02 20:48:12 -0500

    avocadoes

---
+++ avocado.sql
@@ -0,0 +1,65 @@
+--	Learning Module 10 IN-CLASS QUESTIONS ABOUT THE AVOCADO DATA
+
+--	List the average price, sale date, and total volume for all records
+
+SELECT AveragePrice, SalesDate, Total_Volume
+  FROM AvocadoSales;
+
+--	List the unique regions in the Avocado data set
+
+SELECT DISTINCT SalesRegion
+  FROM AvocadoSales;
+
+--	List the unique sales types in the data set sort the results 
+--	alphabetically
+
+SELECT DISTINCT SalesType
+  FROM AvocadoSales
+  ORDER BY SalesType;
+
+--	List the average price, total bags, type, sales year for all sales 
+--	in the Denver region with no extra large bag sales 
+--	(e.g., Xlarge_bags = '0' (zero)) 
+--	NOTE: use Xlarge_bags = '0' because data type is string instead of
+--	numeric. We will learn about changing data types later. 
+
+SELECT AveragePrice, Total_Bags, SalesType, SalesYear
+  FROM AvocadoSales
+  WHERE SalesRegion = 'Denver' AND XLarge_Bags = '0.0';
+
+--	List the region, sales year, and average price for the 15 lowest 
+--	average price records
+
+SELECT TOP 15 SalesRegion, SalesYear, AveragePrice
+  FROM AvocadoSales
+  ORDER BY AveragePrice DESC;
+
+--	List all data for records with average price between $1.00 and 
+--	$1.50 sort by average price
+
+--	OPTION 1
+SELECT *
+  FROM AvocadoSales
+  WHERE AveragePrice >= 1.00 AND AveragePrice <= 1.50
+  ORDER BY AveragePrice;
+
+--	OPTION 2
+SELECT *
+  FROM AvocadoSales
+  WHERE AveragePrice BETWEEN 1.00 AND 1.50
+  ORDER BY AveragePrice;
+
+--	What are the five lowest average prices?
+
+SELECT TOP 5 SalesRegion, SalesYear, AveragePrice
+  FROM AvocadoSales
+  ORDER BY 3;
+
+--	What market region has the highest average price for avocados? Your 
+--	response should only list one record and should include sale date, 
+--	average price, total volume, sales type, and region. 
+--	HINT: Do not use summary queries! We will learn summary queries later.
+
+SELECT TOP 1 SalesDate, AveragePrice, Total_Volume, SalesType, SalesRegion
+  FROM AvocadoSales
+  ORDER BY AveragePrice DESC;

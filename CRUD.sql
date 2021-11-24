ff4474482c1214088dce2b20c3cbc18bcb96bb9a
TrentBrunson <bbiz2@icloud.com>
2021-11-23 20:35:03 -0600

    Receiving stats

---
+++ CRUD.sql
@@ -0,0 +1,11 @@
+-- make copies of tables for analysis with INTO
+-- includes only table & column info; no kets, indexes, etc.
+
+--SELECT select_list (permanent table that's NOT already in DB)
+--INTO table_name
+--FROM table_source
+--[WHERE search_condition]
+--[GROUP BY group_by_list]
+--[HAVING search_condition]
+--[ORDER BY order_by_list]
+
